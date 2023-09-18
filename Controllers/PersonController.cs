5c581d5fc6d2e49a609b55c7227345ad564046ca
manoelcontatosi@gmail.com <manoelcontatosi@gmail.com>
2023-09-18 02:07:24 -0300

    feat : Desenvolvimento questão 3

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -19,11 +19,29 @@ namespace DotNet5Crud.Controllers
 
         public async Task<IActionResult> ListPerson()
         {
-          
-           List<Person> listPerson = new List<Person>();
-           
-           
-            
+             
+           List<Person> listperson = new List<Person>
+              {
+              new Person { Name = "Mahesh Chand", Idade = 23, Email = "Mah@tes.com" },
+              new Person { Name = "Venessa da Mata", Idade = 38, Email = "vanm@tes.com" },
+              
+              new Person { Name = "Bell Marques", Idade = 70 , Email = "bell@tes.com" },
+              
+               new Person { Name = "Claudia Duarte", Idade = 30 , Email = "claudia@emp.com" },
+               
+              new Person { Name = "Belizario Dos Santos", Idade = 30 , Email = "beel@emp.com" }
+              
+              };
+              
+              Lista<Person> filtrado = 
+              listperson.FindAll(p => p. > 30);
+              
+              //Ver output
+              Console.WriteLine(string.Join(',', filtrado));
+              Debug.WriteLine(string.Join(',', filtrado));
+
+              
+              
         }
     } 
   }
